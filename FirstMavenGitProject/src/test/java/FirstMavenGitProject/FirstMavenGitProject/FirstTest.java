8988c63d6875894ed4a95dfa5bf2a3348fa6e311
Ankit <ankitjjain1992@gmail.com>
2018-01-06 12:44:55 +0530

    First Branch commit

--- FirstMavenGitProject/src/test/java/FirstMavenGitProject/FirstMavenGitProject/FirstTest.java
+++ FirstMavenGitProject/src/test/java/FirstMavenGitProject/FirstMavenGitProject/FirstTest.java
@@ -2,13 +2,15 @@ package FirstMavenGitProject.FirstMavenGitProject;
 
 import org.testng.annotations.Test;
 
-public class FirstTest {
+public class FirstTest extends BaseClass
+{
 
 @Test
 public void GmailLogin()
 {
 
-	
+	System.out.println("ankitJain");
+	System.out.println("First Branch");
 }
 	
 }
