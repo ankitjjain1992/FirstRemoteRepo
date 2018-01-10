6ca5b7aa0a40dc6e6851649c25b843ef57f0d922
ankit.jain <ankit.jain@ANJA-BLR-01.accolite.com>
2018-01-10 16:43:01 +0530

    Created Way2A

--- FirstMavenGitProject/src/test/java/FirstMavenGitProject/FirstMavenGitProject/Way2Automation.java
+++ FirstMavenGitProject/src/test/java/FirstMavenGitProject/FirstMavenGitProject/Way2Automation.java
@@ -1,14 +1,16 @@
 package FirstMavenGitProject.FirstMavenGitProject;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class Way2Automation {
+public class Way2Automation extends BaseClass {
 	
 	@Test
 	public void RegisterWay2Automation_Test()
 	{
 		
-		System.out.println("Way2Auto");
+	driver.findElement(By.xpath("//a[text()='Registration']")).click();
+	//driver.findElement(By.xpath())
 	}
 
 }
